b564458e2190608ff6eb5777ae6373f8b1990ef9
gonzicz <gonzicz83@gmail.com>
2019-05-16 18:52:07 +0200

    dodanie profili

---
+++ src/main/java/pl/sda/todoapp/config/DefaultSecurityConfig.java
@@ -0,0 +1,15 @@
+package pl.sda.todoapp.config;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Profile("default")
+public class DefaultSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+    }
+}
